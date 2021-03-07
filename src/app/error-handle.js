33a7c122f057e763335322968ab6c54d140ec3d0
814885760lp <814885760@qq.com>
2021-03-07 19:39:51 +0800

    feat:创建用户已存在校验处理

--- src/app/error-handle.js
+++ src/app/error-handle.js
@@ -4,10 +4,15 @@ const errorHandler = (error, ctx) => {
   let message, status
   switch (error.message) {
     case errorTypes.NAME_OR_PASSWORD_IS_REQUIRED:
-      status = 400
-      message = '用户名或密码不能为空'
+      status = 400 // Bad Request
+      message = '用户名或者密码不能为空~'
       break
-      
+
+    case errorTypes.USER_ALREADY_EXISTS:
+      status = 409 // conflict
+      message = '用户名已经存在~'
+      break
+
     default:
       status = 404
       message = 'NOT FOUND'
--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,11 +1,23 @@
 const errorTypes = require('../constants/error-types')
+const service = require('../service/user.service')
 
 const verifyUser = async (ctx, next) => {
+  // 获取用户名和密码
   const { name, password } = ctx.request.body
+
+  // 用户名和密码不能为空
   if (!name || !password) {
     const error = new Error(errorTypes.NAME_OR_PASSWORD_IS_REQUIRED)
     return ctx.app.emit('error', error, ctx)
   }
+
+  // 用户名不能重复
+  const result = await service.getUserByName(name)
+  if (result.length) {
+    const error = new Error(errorTypes.USER_ALREADY_EXISTS)
+    return ctx.app.emit('error', error, ctx)
+  }
+
   await next()
 }
 
