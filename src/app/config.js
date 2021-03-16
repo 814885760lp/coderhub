1598189bcf057bcfe716573351d78b32b9eca805
814885760lp <814885760@qq.com>
2021-03-16 20:43:33 +0800

    feat:完成了登录和登录验证功能

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,11 @@
 const dotenv = require('dotenv')
+const fs = require('fs')
+const path = require('path')
 
 dotenv.config() // 将.env文件中的变量注入到process.env中
+
+const PRIVATE_KEY = fs.readFileSync(path.resolve(__dirname, './keys/private.key'))
+const PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, './keys/public.key'))
 
 module.exports = {
   APP_PORT,
@@ -10,3 +15,7 @@ module.exports = {
   MYSQL_USER,
   MYSQL_PASSWORD,
 } = process.env // 从process.env中结构出需要的数据
+
+// 如果以下两个导出放在上面导出之前，会被新对象所覆盖，放在下面就是给新的对象添加属性
+module.exports.PRIVATE_KEY = PRIVATE_KEY
+module.exports.PUBLIC_KEY = PUBLIC_KEY
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const { PUBLIC_KEY } = require('../app/config')
+
 const errorTypes = require('../constants/error-types')
 const service = require('../service/user.service')
 const md5password = require('../utils/password-handle')
@@ -5,6 +8,7 @@ const md5password = require('../utils/password-handle')
 const verifyLogin = async (ctx, next) => {
   // 获取用户名和密码
   const { name, password } = ctx.request.body
+
   // 用户名和密码不能为空
   if (!name || !password) {
     const error = new Error(errorTypes.NAME_OR_PASSWORD_IS_REQUIRED)
@@ -14,7 +18,6 @@ const verifyLogin = async (ctx, next) => {
   // 判断用户是否存在
   const result = await service.getUserByName(name)
   const user = result[0]
-  console.log(user);
   if (!user) {
     const error = new Error(errorTypes.USER_DOSE_NOT_EXISTS)
     return ctx.app.emit('error', error, ctx)
@@ -26,9 +29,37 @@ const verifyLogin = async (ctx, next) => {
     return ctx.app.emit('error', error, ctx)
   }
 
+  // user要在验证通过后赋值给ctx
+  ctx.user = user
+
   await next()
+}
+
+const verifyAuth = async (ctx, next) => {
+  const authorization = ctx.request.headers.authorization // 获取token
+
+  // 处理没有token的情况
+  if (!authorization) {
+    const error = new Error(errorTypes.UNAUTHORIZATION)
+    return ctx.app.emit('error', error, ctx)
+  }
+
+  const token = authorization.replace('Bearer ', '') // 截取出来真正的token6
+  try {
+    // 验证token
+    const result = jwt.verify(token, PUBLIC_KEY, {
+      algorithms: ['RS256'],
+    })
+    ctx.user = result
+
+    await next()
+  } catch (err) {
+    const error = errorTypes.UNAUTHORIZATION
+    return new Error('error', error, ctx)
+  }
 }
 
 module.exports = {
   verifyLogin,
+  verifyAuth,
 }
